9b54b8fd37c0684804073dcc7db0f9af165c0b15
Chart Chongcharoen <chartchuo@gmail.com>
2018-12-12 19:45:11 +0700

    add list

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,89 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  final items = List<String>.generate(0, (i) => "Item ${i + 1}");
+  // List<String> items;
+  Future<String> _barcodeString;
+
+  @override
+  Widget build(BuildContext context) {
+    final title = 'QR Code Reader';
+
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return Dismissible(
+              // Each Dismissible must contain a Key. Keys allow Flutter to
+              // uniquely identify Widgets.
+              key: Key(item),
+              // We also need to provide a function that tells our app
+              // what to do after an item has been swiped away.
+              onDismissed: (direction) {
+                // Remove the item from our data source.
+                setState(() {
+                  items.removeAt(index);
+                });
+
+                // Then show a snackbar!
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text("$item dismissed")));
+              },
+              // Show a red background as the item is swiped away
+              background: Container(color: Colors.red),
+              child: ListTile(title: Text('$item')),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _barcodeString = QRCodeReader()
+                .setAutoFocusIntervalInMs(200)
+                .setForceAutoFocus(true)
+                .setTorchEnabled(true)
+                .setHandlePermissions(true)
+                .setExecuteAfterPermissionGranted(true)
+                .scan();
+            _barcodeString.then((String str) {
+              if(str==null) return;
+              setState(() {
+                items.add(str);
+              });
+            });
+          },
+          tooltip: 'Reader the QRCode',
+          child: Icon(Icons.add_a_photo),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'QRCode Reader Demo',
+      title: 'QRCode Reader',
       home: new MyHomePage(),
     );
   }
